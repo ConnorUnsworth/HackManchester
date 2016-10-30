cdd08f762cc121a6a7f1ef257c0df01341da729c
Anton <A.Wolfarth@hotmail.com>
2016-10-30 01:24:56 +0100

    image encode and decode fully working

--- src/image/ImgTestApp.java
+++ src/image/ImgTestApp.java
@@ -1,6 +1,8 @@
 package image;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
 
 import javax.imageio.ImageIO;
@@ -16,9 +18,10 @@ public class ImgTestApp extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		System.out.println("Expanding");
 		ImageExpander imgex = new ImageExpander();
+		ImageDataEncoder dataenc = new ImageDataEncoder();
 		stage.show();
+		
 
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
@@ -32,7 +35,44 @@ public class ImgTestApp extends Application{
 		data[2]=105;
 		data[3]=46;
 		
-		imgex.expand(bufferedImage, data);
+		System.out.println("Expanding");
+		BufferedImage bi1 = imgex.expand(bufferedImage);
+		System.out.println("   ");
+		
+		BufferedImage bi2 = dataenc.encode(bi1, data);
+		
+		//clone
+		BufferedImage bi0 = new BufferedImage(bi2.getWidth(), bi2.getHeight(), java.awt.image.BufferedImage.TYPE_INT_RGB);
+		WritableRaster raster0 = bi0.getRaster();
+		Raster raster2 = bi2.getRaster();
+		for(int x = 0; x < bi2.getWidth(); x++){
+			for(int y = 0; y < bi2.getHeight(); y++){
+				int[] inarr = new int[4];
+				int[] outarr = new int[4];
+				raster2.getPixel(x, y, inarr);
+				outarr[0] = inarr[0];
+				outarr[1] = inarr[1];
+				outarr[2] = inarr[2];
+				outarr[3] = inarr[3];
+				
+				raster0.setPixel(x, y, outarr);
+			}
+		}
+		
+		//dataenc.outputImageData(bi2);
+		
+		//DECODE bi2 is what is posted
+		BufferedImage bi3 = imgex.recoverKeyImage(bi0);
+		
+		BufferedImage bi4 = imgex.expand(bi3);
+		//dataenc.outputImageData(bi4);
+		
+		
+		Byte[] dataOutput = dataenc.decodeImage(bi4, bi2);
+		
+		System.out.println(dataOutput[0].toString() + ", "+ dataOutput[1].toString() + ", "+ dataOutput[2].toString() + ", "+ dataOutput[3].toString() + ", "+ dataOutput[4].toString());
+		
+		
 
 	}
 }
