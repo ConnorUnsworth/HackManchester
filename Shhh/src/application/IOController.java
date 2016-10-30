982e54e6a99612d0f944d30f5952246a586e4ca3
Thomas Kneller <thomas.kneller@yahoo.com>
2016-10-30 01:26:27 +0100

    Fixed Saving Image
    ^^

--- Shhh/src/application/IOController.java
+++ Shhh/src/application/IOController.java
@@ -5,121 +5,159 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.VBoxBuilder;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
- 
+
 public class IOController extends Application {
-	
+
 	private Stage stage;
-	
+
 	private File outputfile;
-	
-	
+
+
 	public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-    	this.stage = primaryStage;
-        Group root = new Group();
-        getBufferedImage();
-    }
-        
-    public CustomBufferedImageStorage getBufferedImage()
-    {
-    	 FileChooser fileChooser = new FileChooser();
-         FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
-         fileChooser.getExtensionFilters().add(extFilterJpg);
-         File file = fileChooser.showOpenDialog(stage);
-         
-         try{
-         	BufferedImage bufferedImage = ImageIO.read(file);
-         	CustomBufferedImageStorage imageStorage = new CustomBufferedImageStorage(bufferedImage, file.getAbsolutePath());
-         	return imageStorage;
-         }
-         catch(Exception ex)
-         {
-         	return null;
-         	
-         }
-         
-    }
-    
-    public File getConvertedFile()
-    {
-    	return outputfile;
-    }
-    
-    
-    
-    
-    public void saveNewImage(BufferedImage image)
-    {
-    	try {
-    	    // retrieve image
-    	     outputfile = new File("c:\\image\\saved.png");
-    	    ImageIO.write(image, "png", outputfile);
-    	} catch (IOException e) {
-    	    
-    	}
-    }
-    
-    public Image getNewImage()
-    {
-    
-    	
-    	 try{
-    		File resizedFile = new File("c:\\image\\saved.png");
-      	    
-      	 	Image image = new Image(resizedFile.toURI().toString());
-      	 	
-          	return image;
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.stage = primaryStage;
+		Group root = new Group();
+		getBufferedImage();
+	}
+
+	public CustomBufferedImageStorage getBufferedImage()
+	{
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
+		fileChooser.getExtensionFilters().add(extFilterJpg);
+		File file = fileChooser.showOpenDialog(stage);
+
+		try{
+			BufferedImage bufferedImage = ImageIO.read(file);
+			CustomBufferedImageStorage imageStorage = new CustomBufferedImageStorage(bufferedImage, file.getAbsolutePath());
+			return imageStorage;
+		}
+		catch(Exception ex)
+		{
+			return null;
+
+		}
+
+	}
+
+	public File getConvertedFile()
+	{
+		return outputfile;
+	}
+
+
+
+
+	public void tempSaveNewImage(BufferedImage image)
+	{
+//		 FileChooser fileChooser = new FileChooser();
+//         fileChooser.setTitle("Save Image");
+//     	FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
+//		fileChooser.getExtensionFilters().add(extFilterJpg);
+//         File file = fileChooser.showSaveDialog(stage);
+//         
+//         if (file != null) {
+//             try {
+//                 ImageIO.write(image, "png", file);
+//
+//             } catch (IOException ex) {
+//                 System.out.println(ex.getMessage());
+//             }
+//         }
+		
+		
+		
+		try {
+			// retrieve image
+			outputfile = new File("c:\\image\\saved.png");
+			ImageIO.write(image, "png", outputfile);
+		} catch (IOException e) {
+
+		}
+	}
+	
+	public void saveImgToPath(Image image)
+	{
+		File tempFile = outputfile;
+		BufferedImage bufferedImage = null;
+		try {
+			bufferedImage = ImageIO.read(tempFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		 FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Save Image");
+  	FileChooser.ExtensionFilter extFilterJpg = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
+		fileChooser.getExtensionFilters().add(extFilterJpg);
+      File file = fileChooser.showSaveDialog(stage);
+      
+      if (file != null) {
+          try {
+              ImageIO.write(bufferedImage, "png", file);
+
+          } catch (IOException ex) {
+              System.out.println(ex.getMessage());
           }
-          catch(Exception ex)
-          {
-          	return null;
-          	
-          }
-    }
-    
-    
-    
-    
-    public CustomSoundStorage getSound()
-    {
-   	 FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilterAud = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
-        fileChooser.getExtensionFilters().add(extFilterAud);
-        File file = fileChooser.showOpenDialog(stage);
-        
-        try{
-        	Media sound = new Media(file.getAbsolutePath());
-        	CustomSoundStorage soundStorage = new CustomSoundStorage(sound, file.getAbsolutePath());
-        	return soundStorage;
-        }
-        catch(Exception ex)
-        {
-        	return null;
-        	
-        }
-        
-   }
-    
-    
-    
-    
+      }
+	}
+	
+
+	public Image getNewImage()
+	{
+
+
+		try{
+			File resizedFile = new File("c:\\image\\saved.png");
+
+			Image image = new Image(resizedFile.toURI().toString());
+
+			return image;
+		}
+		catch(Exception ex)
+		{
+			return null;
+
+		}
+	}
+
+
+
+
+	public CustomSoundStorage getSound()
+	{
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilterAud = new FileChooser.ExtensionFilter("JPG (*.jpg)(*.png)", "*.jpg","*.png");
+		fileChooser.getExtensionFilters().add(extFilterAud);
+		File file = fileChooser.showOpenDialog(stage);
+
+		try{
+			Media sound = new Media(file.getAbsolutePath());
+			CustomSoundStorage soundStorage = new CustomSoundStorage(sound, file.getAbsolutePath());
+			return soundStorage;
+		}
+		catch(Exception ex)
+		{
+			return null;
+
+		}
+
+	}
+
+
+
+
 }
--- Shhh/src/application/Main.java
+++ Shhh/src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
-	
+
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import image.ImageExpander;
 import javafx.application.Application;
@@ -26,70 +29,73 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application implements Initializable{
+
+
+	@FXML
+	private Button btnBrowseImage;
+	@FXML
+	private Button btnBrowseSound;
+	@FXML
+	private Button btnTwitterSignIn;
+	@FXML
+	private Button btnTweetImage;
+	@FXML
+	private Button btnSaveImage;
+
+	@FXML
+	private Button btnHideMessage;
+
+	@FXML
+	private TextField txtImagePath;
+	@FXML
+	private TextField txtSoundPath;
+
+	@FXML
+	private TextArea txtMessageToHide;
+
+	@FXML
+	private ImageView imgLoadedImage;
+	@FXML
+	private ImageView imgHiddenMessage;
+
+	@FXML
+	private Text txtErrMsg;
+
+
+	IOController ioController = new IOController();
+	CustomBufferedImageStorage storage;
+	JavaTweet tweet = new JavaTweet();
 	
-
-@FXML
-private Button btnBrowseImage;
-@FXML
-private Button btnBrowseSound;
-@FXML
-private Button btnTwitterSignIn;
-@FXML
-private Button btnTweetImage;
-
-@FXML
-private Button btnHideMessage;
-
-@FXML
-private TextField txtImagePath;
-@FXML
-private TextField txtSoundPath;
-
-@FXML
-private TextArea txtMessageToHide;
-
-@FXML
-private ImageView imgLoadedImage;
-@FXML
-private ImageView imgHiddenMessage;
-
-@FXML
-private Text txtErrMsg;
-
-	
-IOController ioController = new IOController();
-CustomBufferedImageStorage storage;
-JavaTweet tweet = new JavaTweet();
 
 	@Override
 	public void start(Stage primaryStage) {
-		
 
-		
+
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.getResource("/MainUI.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
-		
+
 			Scene scene = new Scene(root, 600, 350);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Shhh");
 			primaryStage.setScene(scene);
-			
+
 
 			primaryStage.setOnCloseRequest(e -> Platform.exit());
 
 			primaryStage.show();
-			
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	public static void main(String[] args) {
-		
+
 		launch(args);
 	}
 
@@ -97,95 +103,107 @@ JavaTweet tweet = new JavaTweet();
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-	
-	
-		
+
+
+
 		btnBrowseImage.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				
+
 				importImage();
 			}
 		});
-		
+
 		btnBrowseSound.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				
+
 				importSound();
 			}
 		});
-		
+
 		btnHideMessage.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				
+
 				saveEncryptedMessage();
+				
+				btnTwitterSignIn.setDisable(false);
+				btnTweetImage.setDisable(false);
 			}
 		});
 		
+		btnSaveImage.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+
+				ioController.saveImgToPath(imgHiddenMessage.getImage());
+			}
+		});
+
+		Image twitterIcon = new Image(getClass().getResourceAsStream("resources/twitter.png"));
+		btnTwitterSignIn.setGraphic(new ImageView(twitterIcon));
 		btnTwitterSignIn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				
-				
+
+
 				tweet.oauth();
 			}
 		});
-		
+
 		btnTweetImage.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
-				
+
 				tweet.tweet(ioController.getConvertedFile());
 			}
 		});
-			
+
 	}
-
-
-	
 
 	protected void saveEncryptedMessage() {
 
 		System.out.println("ENCRYPT MESSAGE");
 		//ANTON AND ALEX
-		
+
 		ImageExpander iExpander = new ImageExpander();
 		
 		try {
-		ioController.saveNewImage(iExpander.expand(storage.getImage()));
-					
-		} catch (IOException e) {
+			ioController.tempSaveNewImage(iExpander.expand(storage.getImage()));
 			
+		} catch (IOException e) {
+
 		}
-		
+
 		imgHiddenMessage.setImage(ioController.getNewImage());
 
-		
+
 		txtErrMsg.setVisible(true);
 		txtErrMsg.setText("Message Hidden");
-		
+
 	}
+	
+
 
 
 	protected void importImage() {
-	 	storage = ioController.getBufferedImage();
-	 	txtImagePath.setText(storage.getFilePath());
-	 	File file =new File(storage.getFilePath());
-	 	Image image = new Image(file.toURI().toString());
-	 	imgLoadedImage.setImage(image);
-	 	
+		storage = ioController.getBufferedImage();
+		txtImagePath.setText(storage.getFilePath());
+		File file =new File(storage.getFilePath());
+		Image image = new Image(file.toURI().toString());
+		imgLoadedImage.setImage(image);
+		
+		btnHideMessage.setDisable(false);
+
 	}
-	
+
 	protected void importSound() {
 		// TODO Auto-generated method stub
 		System.out.println("Import Audio File");
 		CustomSoundStorage storage = ioController.getSound();
 		txtSoundPath.setText(storage.getFilePath());
-		
-	 	File file =new File(storage.getFilePath());
 
+		File file = new File(storage.getFilePath());
 
 		//Load Sound into here
-		
+
 	}
 
-	
+
 
 }
